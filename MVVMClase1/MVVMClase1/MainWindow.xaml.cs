25241dc87fbde7ac4595c7ea7645af5416585844
Agustin <agustashd@gmail.com>
2018-05-04 18:07:39 -0300

    cosas

---
+++ MVVMClase1/MVVMClase1/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MVVMClase1.Model;
+using System.Windows;
+
+namespace MVVMClase1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var persona = new Persona();
+            persona.Apellido = "Lopez";
+            persona.Contador = -10;
+        }
+    }
+}
---
+++ MVVMClase1/MVVMClase1/Model/Persona.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMClase1.Model
+{
+    public class Persona : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Propiedad CLR
+        int contador;
+        public int Contador {
+            get { return contador; }
+            set
+            {
+                if (value < 0)
+                    throw new Exception("No se admiten valores negativos");
+                contador = value;
+                OnPropertyChange("Contador");
+            }
+        }
+
+        string nombre;
+        public string Nombre
+        {
+            get { return nombre; }
+            set
+            {
+                nombre = value;
+                OnPropertyChange("Nombre");
+            }
+        }
+
+        string apellido;
+        public string Apellido
+        {
+            get { return apellido; }
+            set
+            {
+                apellido = value;
+                OnPropertyChange("Apellido");
+            }
+        }
+
+        // Auto Propiedades
+        public int ID { get; set; }
+
+        private void OnPropertyChange(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
